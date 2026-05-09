4eff1642e87674098eb31acd4838ebe6356b6abc
UDIE322 <mr.spirin.kirysha@gmail.com>
2026-05-09 21:47:45 +0300

    s try

--- include/textgen.h
+++ include/textgen.h
@@ -1,3 +1,4 @@
+// Copyright 2026 b83
 #pragma once
 #include <string>
 #include <deque>
@@ -6,15 +7,14 @@
 
 const int NPREF = 2;
 const int MAXGEN = 1000;
-const std::string NONWORD = "\n";
+const char NONWORD[] = "\n";
 
 typedef std::deque<std::string> Prefix;
 
 class TextGen {
 public:
-    std::map<Prefix, std::vector<std::string>> statetab;
-
-    void add(Prefix& pref, const std::string& word);
-    void build(std::istream& in);
-    void generate(std::ostream& out, int maxwords = MAXGEN);
+	std::map<Prefix, std::vector<std::string>> statetab;
+	void add(Prefix& pref, const std::string& word);
+	void build(std::istream& in);
+	void generate(std::ostream& out, int maxwords = MAXGEN);
 };
